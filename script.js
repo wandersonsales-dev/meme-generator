c4bec2d2f0cfb87697afe7a93ff5207a14230a72
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-13 01:08:18 -0300

    :rocket: [Req. 02] - Carregar Imagem

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const idContainerMeme = 'meme-image-container';
+const idMemeImage = 'meme-image';
+
 function addText() {
   const inputText = document.getElementById('text-input');
   inputText.addEventListener('keyup', (e) => {
@@ -6,6 +9,45 @@ function addText() {
   });
 }
 
+function checkValueInput() {
+  const inputText = document.getElementById('text-input');
+  if (inputText.value === '') {
+    const memeText = document.getElementById('meme-text');
+    memeText.innerHTML = 'Gere aqui seu meme';
+  }
+}
+
+function loadImage() {
+  const inputImage = document.getElementById('meme-insert');
+  inputImage.addEventListener('change', (e) => {
+    checkValueInput();
+    if (!document.getElementById(idMemeImage)) {
+      const memeContainer = document.getElementById(idContainerMeme);
+      const newImg = document.createElement('img');
+      newImg.id = idMemeImage;
+      newImg.style.maxWidth = '100%';
+      newImg.style.maxHeight = '100%';
+      memeContainer.appendChild(newImg);
+      memeContainer.style.height = newImg.style.width;
+    }
+    const newImg = document.getElementById(idMemeImage);
+    newImg.src = URL.createObjectURL(e.target.files[0]);
+    newImg.onload = () => {
+      URL.revokeObjectURL(newImg.src);
+    };
+  });
+}
+
+function checkValueImg() {
+  if (!document.getElementById(idMemeImage)) {
+    const containerImg = document.getElementById(idContainerMeme);
+    containerImg.style.height = '500px';
+  }
+}
+
 window.onload = () => {
   addText();
+  loadImage();
+  checkValueImg();
+  checkValueInput();
 };
